41b2ac48b7c3ca5d70fc65b60e5f9e27d5fcc9b2
MilesBradt <smbradtmichael@gmail.com>
2019-11-21 11:47:57 -0800

    Wed lessons.

---
+++ src/components/App.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Admin from './Admin';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: [],
+      selectedTicket: null,
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    5000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime() {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  handleChangingSelectedTicket(ticket){
+    this.setState({selectedTicket: ticket});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname}
+          onTicketSelection={this.handleChangingSelectedTicket}
+          selectedTicket={this.state.selectedTicket}/>} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
+}
+
+export default App;
